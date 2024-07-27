c2530e862385ce1a4552389d6bd047174d345ec3
navneetjoshi223 <joshi.nav@northeastern.edu>
2024-07-26 20:17:39 -0400

    Refactored APIs with async await

--- frontend/src/components/CompanyDetails/CompanyDetailsPage.jsx
+++ frontend/src/components/CompanyDetails/CompanyDetailsPage.jsx
@@ -5,23 +5,42 @@ function CompanyDetailsPage() {
   const { id } = useParams();
   const [company, setCompany] = useState(null);
   const [locations, setLocations] = useState([]);
+  const [error, setError] = useState(null);
   const apiUrl = process.env.REACT_APP_API_URL + '/companies';
 
   useEffect(() => {
-    fetch(`${apiUrl}/${id}`)
-      .then(response => response.json())
-      .then(data => {
-        console.log('Company details', data);
-        setCompany(data);
-
-        fetch(`${apiUrl}/${id}/locations`)
-            .then(response => response.json())
-            .then(data => {
-                console.log('Location details for the company', data);
-                setLocations(data);
-            })
-      })
-      .catch(error => console.error('Error fetching company details:', error));
+    const fetchCompanyDetails = async () => {
+        try {
+          const response = await fetch(`${apiUrl}/${id}`);
+          if (!response.ok) {
+            throw new Error('There was a network error');
+          }
+          const data = await response.json();
+          console.log('Company details', data);
+          setCompany(data);
+        } catch (error) {
+          console.error('Error fetching company details:', error);
+          setError(error.toString());
+        }
+      };
+  
+      const fetchCompanyLocations = async () => {
+        try {
+          const response = await fetch(`${apiUrl}/${id}/locations`);
+          if (!response.ok) {
+            throw new Error('There was a network error');
+          }
+          const data = await response.json();
+          console.log('Location details for the company', data);
+          setLocations(data);
+        } catch (error) {
+          console.error('Error fetching location details for the company:', error);
+          setError(error.toString());
+        }
+      };
+  
+      fetchCompanyDetails();
+      fetchCompanyLocations();
   }, [id]);
 
   if (!company) {
